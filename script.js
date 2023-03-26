ccaa78b3134d1d49e126b8640eac7001f89fd0b2
Ogo43 <emehaogochukwu@gmail.com>
2023-03-27 00:30:46 +0100

    Todo_list

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 //declare functions for add, edit, delete
 
-let task = document.getElementById("todo_task");
-const inputText = document.getElementById("todo-input");
-let addTask = document.getElementsById("add-input");
-let editTask = document.getElementsById("edit-task");
-let deleteTask = document.getElementsById("delete-task")
+const task = document.getElementById("todo_task");
+let inputText = document.getElementById("todo-input");
+let addTask = document.getElementById("add-input");
+let editTask = document.getElementById("edit-task");
+let deleteTask = document.getElementById("delete-task")
+let todo_wrapper = document.querySelector(".unordered-list");
 
+let todo_arr = []
 
-inputText.addEventListener("changeable", (text) => {
-    return text;  
+let generateTemplate = ()=>{
+    
+    todo_wrapper.innerHTML = "";
+
+    let todo_markup_arr =  todo_arr.map((el, index)=>{
+        return `
+        <li class="list" id=${index}>
+        <input 
+        type="text" 
+        value=${el}
+        disabled
+        class="editable-input"
+        />
+    
+        <div class="btn-wrapper">
+        <button class="edit" id="edit-task"><i class='bx bx-edit' ></i>Edit</button> 
+        <button class="delete" id="delete-task"><i class='bx bx-trash'></i>Delete</button>
+        </div> 
+        </li>
+    `
+     })
+    todo_markup_arr.forEach(el=>{
+        todo_wrapper.insertAdjacentHTML("beforeend", el)    
+    })
+}
+
+inputText.addEventListener("change", (e) => {
+    e.preventDefault();
+     todo_arr.push(e.target.value);
+     e.target.value="";
+     
+
+    generateTemplate();
 })
 
-addTask.addEventListener("click", (listOfTask) => {
-    for (let i = 0; i < listOfTask.length; i++) {
-        let output = [];
-        const singleList = listOfTask[i];
-        output.push(singleList);    
-    }
-    return output;
-})
+todo_wrapper.addEventListener("click", (e)=>{
+    // console.log(e.target.parentNode.parentNode.id)
+    // console.log(typeof parseInt(e.target.parentNode.parentNode.id))
+    // console.log(e.bubbles)
+    let ID = parseInt(e.target.parentNode.parentNode.id)
 
-const list = addTask.addEventListener("click", ("Groceries"));
-console.log(list);
-
-editTask.addEventListener("click", (editableTask) => {
-
-})
-
-deleteTask.addEventListener("click", (deleteOfTask) => {
-    addTask.addEventListener("click", (listOfTask));
-    prompt("Delete successful");
+    let new_todo_arr = todo_arr.slice(0, ID).concat(todo_arr.slice(ID + 1))
+    todo_arr = new_todo_arr
+    generateTemplate()
 })
